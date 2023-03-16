41aa478ce70dca40b8b21d3af7d4418a89c07834
pedro <pedroh.mix@gmail.com>
2023-03-16 18:47:53 -0300

    initial commit

--- src/app/layouts/admin-layout/admin-layout.module.ts
+++ src/app/layouts/admin-layout/admin-layout.module.ts
@@ -17,20 +17,22 @@ import {MatRippleModule} from '@angular/material/core';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatTooltipModule} from '@angular/material/tooltip';
 import {MatSelectModule} from '@angular/material/select';
+import {MatAutocompleteModule} from "@angular/material/autocomplete";
 
 @NgModule({
-  imports: [
-    CommonModule,
-    RouterModule.forChild(AdminLayoutRoutes),
-    FormsModule,
-    ReactiveFormsModule,
-    MatButtonModule,
-    MatRippleModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatSelectModule,
-    MatTooltipModule,
-  ],
+    imports: [
+        CommonModule,
+        RouterModule.forChild(AdminLayoutRoutes),
+        FormsModule,
+        ReactiveFormsModule,
+        MatButtonModule,
+        MatRippleModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatSelectModule,
+        MatTooltipModule,
+        MatAutocompleteModule,
+    ],
   declarations: [
     DashboardComponent,
     UserProfileComponent,
--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -1,40 +1,58 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {CepService} from "../services/cep.service";
-
+import {FormControl} from "@angular/forms";
+import {Observable} from "rxjs";
+import {map, startWith} from 'rxjs/operators';
+import {estados} from "../constants/estados";
 @Component({
-  selector: 'app-user-profile',
-  templateUrl: './user-profile.component.html',
-  styleUrls: ['./user-profile.component.css']
+    selector: 'app-user-profile',
+    templateUrl: './user-profile.component.html',
+    styleUrls: ['./user-profile.component.css']
 })
 export class UserProfileComponent implements OnInit {
 
-  constructor(private cepService: CepService) { }
-
-  estado: string = '';
-  cidade: string = '';
-  pais: string = '';
-  endereco: string = '';
-
-  ngOnInit() {
-  }
-
-  checkCep(event) {
-    if(event.length >= 8)
-      this.cepService.getCep(event).subscribe(value => {
-        console.log(value)
-        this.clearValues();
-        this.estado = value.uf;
-        this.cidade = value.localidade;
-        this.pais = 'Brasil';
-        this.endereco = value.logradouro;
-      })
+    constructor(private cepService: CepService) {
     }
 
-  private clearValues() {
-    this.estado = '';
-    this.cidade = '';
+    estado: string = '';
+    cidade: string = '';
+    pais: string = '';
+    endereco: string = '';
+    numero: number;
 
-    this.pais = '';
-    this.endereco= '';
-  }
+    myControl = new FormControl('');
+    options: string[] = estados.map(value => value.nome);
+    filteredOptions: Observable<string[]>;
+    ngOnInit() {
+        this.filteredOptions = this.myControl.valueChanges.pipe(
+            startWith(''),
+            map(value => this._filter(value || '')),
+        );
+    }
+
+    private _filter(value: string): string[] {
+        const filterValue = value.toLowerCase();
+
+        return this.options.filter(option => option.toLowerCase().includes(filterValue));
+    }
+
+    checkCep(event) {
+        if (event.length >= 8)
+            this.cepService.getCep(event).subscribe(value => {
+                console.log(value)
+                this.clearValues();
+                this.estado = value.uf;
+                this.cidade = value.localidade;
+                this.pais = 'Brasil';
+                this.endereco = value.logradouro;
+            })
+    }
+
+    private clearValues() {
+        this.estado = '';
+        this.cidade = '';
+
+        this.pais = '';
+        this.endereco = '';
+    }
 }
